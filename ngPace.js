831b161777d3665704d8d0a07ccc674272ab3a3d
yi-chan-chiang <cymsjh350097@gmail.com>
2015-03-10 14:30:14 +0800

    add lib
    angularJS module

---
+++ ngPace.js
@@ -0,0 +1,41 @@
+'use strict';
+
+angular.module('autoLoad.pace', [])
+    .factory('ngPace', ['$timeout', '$window', '$rootScope', function ($timeout, $window, $rootScope) {
+        var pace = $window.Pace;
+        //public methods
+        $rootScope.ngPace = {};
+        $rootScope.ngPace.isloading = true;
+        $rootScope.ngPace.visble = {'true': 'visble'};
+        var targetElement = document.querySelector('body');
+        this.el = document.createElement('div');
+        this.el.className = "ng-pace";
+        this.el.innerHTML = '<div class="pace-mask" ng-class="ngPace.visble[!ngPace.isloading]"></div>';
+        targetElement.appendChild(this.el);
+        var self = {
+            pace: pace,
+            startloading: function () {
+                $rootScope.$apply(function () {
+                    $rootScope.ngPace.isloading = true;
+                    $rootScope.ngPace.iserror = false;
+                    $rootScope.ngPace.isnone = false;
+                });
+            },
+            endloading: function () {
+                $rootScope.$apply(function () {
+                    $rootScope.ngPace.isloading = false;
+                    $rootScope.ngPace.iserror = false;
+                    $rootScope.ngPace.isnone = false;
+                });
+            }
+        }
+        self.pace.on("start", function () {
+            self.startloading();
+        });
+
+        self.pace.on("done", function () {
+            self.endloading();
+        });
+        return self;
+    }]);
+
